4e9e1a7908e412d689839880ca8925620183f2c7
Victor Raul Gutierrez Sanabria <gutierrezsanabria@gmail.com>
2025-03-23 18:32:54 -0500

    Verificacion de preguntas

--- questions.js
+++ questions.js
@@ -1,242 +1,255 @@
 export const questions = [
-    
-      
-      
-            {
-                question : "En Doctrina y Convenios 28, ¿qué se enseña sobre la autoridad en la Iglesia?",
-                options: [
-                    "(A) Solo el profeta puede recibir revelaciones para la Iglesia",
-                    "(B) Todos los miembros pueden recibir revelaciones personales",
-                    "(C) Los líderes locales tienen la misma autoridad que el profeta",
-                    "(D) La autoridad se basa en la sabiduría humana"
-                ],
-                "correct": 0
-            },
-            {
-                question : "Según Doctrina y Convenios 1-30, ¿qué se enseña sobre la inteligencia?",
-                options: [
-                    "(A) Es un don temporal",
-                    "(B) Es eterna y no creada",
-                    "(C) Depende del conocimiento humano",
-                    "(D) Es un atributo solo de Dios"
-                ],
-                "correct": 1
-            },
-            {
-                question : "En Doctrina y Convenios 4, ¿qué cualidades son necesarias para servir en la obra misional?",
-                options: [
-                    "(A) Conocimiento científico y habilidades técnicas",
-                    "(B) Riqueza, fama y poder",
-                    "(C) Fe, esperanza, caridad y amor", 
-                    "(D) Experiencia en liderazgo empresarial"
-                ],
-                "correct": 2
-            },
-            {
-                question : "Según Doctrina y Convenios 1-37, ¿qué visión tuvo José Smith sobre el reino celestial?",
-                options: [
-                    "(A) La visión de los tres grados de gloria",
-                    "(B) La visión del bautismo por los muertos",
-                    "(C) La visión del Libro de Mormón",
-                    "(D) La visión del fin del mundo"
-                ],
-                "correct": 1
-            },
-            {
-                question : "En Doctrina y Convenios 6, ¿qué don promete el Señor a Oliver Cowdery?",
-                options: [
-                    "(A) El don de traducir",
-                    "(B) El don de profecía",
-                    "(C) El don de sanación",
-                    "(D) El don de lenguas"
-                ],
-                "correct": 0
-            },
-            {
-                question : "Según Doctrina y Convenios 8, ¿cómo se recibe la revelación?",
-                options: [
-                    "(A) Mediante el estudio y la fe",
-                    "(B) Mediante el poder del Espíritu Santo",
-                    "(C) Mediante sueños y visiones",
-                    "(D) Mediante la lógica y el razonamiento"
-                ],
-                "correct": 1
-            },
-            {
-                question : "En Doctrina y Convenios 10, ¿qué estrategia usa Satanás para destruir la obra de Dios?",
-                options: [
-                    "(A) Destruir físicamente las iglesias",
-                    "(B) Engañar a los líderes de la Iglesia",
-                    "(C) Crear divisiones entre los santos",
-                    "(D)  Robar y alterar las escrituras"
-                ],
-                "correct": 0
-            },
-            {
-                question : "Según Doctrina y Convenios 13, ¿qué autoridad fue restaurada por Juan el Bautista?",
-                options: [
-                    "(A) El Sacerdocio de Melquisedec",
-                    "(B) El Sacerdocio Aarónico ",
-                    "(C) Las llaves del reino",
-                    "(D) El don de profecía"
-                ],
-                "correct": 1
-            },
-            {
-                question : "En Doctrina y Convenios 14, ¿qué promete el Señor a quienes trabajan en Su obra?",
-                options: [
-                    "(A) Poder político",
-                    "(B) Riquezas materiales",
-                    "(C) Fama y reconocimiento",
-                    "(D) Vida eterna "
-                ],
-                "correct": 3
-            },
-            {
-                question : "Según Doctrina y Convenios 17, ¿qué se les prometió a los Tres Testigos del Libro de Mormón?",
-                options: [
-                    "(A) Recibir visiones del futuro",
-                    "(B) Recibir el sacerdocio",
-                    "(C) Ser traductores de las escrituras",
-                    "(D) Ver las planchas de oro "
-                ],
-                "correct": 3
-            },
-            {
-                question : "En Doctrina y Convenios 18, ¿qué enseña el Señor sobre el valor de las almas?",
-                options: [
-                    "(A) Que todas las almas son iguales",
-                    "(B) Que son de gran valor para Dios ",
-                    "(C) Que solo algunas almas son valiosas",
-                    "(D) Que el valor de las almas depende de sus obras"
-                ],
-                "correct": 1
-            },
-            {
-                question : "Según Doctrina y Convenios 19, ¿qué advertencia se da sobre el pecado?",
-                options: [
-                    "(A) Que el pecado es inevitable",
-                    "(B) Que el pecado no tiene consecuencias",
-                    "(C) Que el pecado trae sufrimiento ",
-                    "(D) Que el pecado solo afecta a los demás"
-                ],
-                "correct": 2
-            },
-            {
-                question : "En Doctrina y Convenios 23, ¿qué consejo da el Señor a los líderes de la Iglesia?",
-                options: [
-                    "(A) Evitar la responsabilidad",
-                    "(B) Buscar riquezas y poder",
-                    "(C) Orar y predicar con sinceridad ",
-                    "(D) Ignorar a los miembros necesitados"
-                ],
-                "correct": 2
-            },
-            {
-                question : "Según Doctrina y Convenios 25, ¿cuál es el papel de Emma Smith en la Iglesia?",
-                options: [
-                    "(A) Ser una líder y ejemplo para las mujeres",
-                    "(B) Ser la presidenta de la Sociedad de Socorro",
-                    "(C) Ser la escriba de las revelaciones",
-                    "(D) Ser la traductora del Libro de Mormón"
-                ],
-                "correct": 0
-            },
-            {
-                question : "En Doctrina y Convenios 27, ¿qué simboliza la armadura de Dios?",
-                options: [
-                    "(A) El poder político",
-                    "(B) La fuerza física",
-                    "(C) La riqueza material",
-                    "(D) La protección espiritual "
-                ],
-                "correct": 3
-            },
-            {
-                question : "Según Doctrina y Convenios 33, ¿qué llama el Señor a hacer a los élderes de la Iglesia?",
-                options: [
-                    "(A) Predicar el evangelio",
-                    "(B) Construir templos",
-                    "(C) Recaudar fondos",
-                    "(D) Escribir nuevas escrituras"
-                ],
-                "correct": 0
-            },
-            {
-                question : "En Doctrina y Convenios 1-37, ¿quiénes pueden heredar la salvación en el reino celestial?",
-                options: [
-                    "(A) Solo los bautizados en esta vida",
-                    "(B) Aquellos que mueren sin conocer el evangelio ",
-                    "(C) Aquellos que han recibido todas las ordenanzas",
-                    "(D) Solo los profetas y apóstoles"
-                ],
-                "correct": 1
-            },
-            {
-                question : "Según Doctrina y Convenios 1-38, ¿qué se revela sobre el mundo de los espíritus?",
-                options: [
-                    "(A) Que es un lugar de descanso sin actividad",
-                    "(B) Que es un lugar de castigo eterno",
-                    "(C) Que es un lugar de enseñanza y preparación ",
-                    "(D) Que es un lugar de reunión familiar"
-                ],
-                "correct": 2
-            },
-            {
-                question : "En Doctrina y Convenios 19, ¿qué significa la palabra 'eterno' en el contexto del castigo de Dios?",
-                options: [
-                    "(A) Un sufrimiento físico eterno",
-                    "(B) Un castigo sin fin",
-                    "(C) Un período de tiempo limitado pero intenso ",
-                    "(D) Un estado de separación permanente"
-                ],
-                "correct": 2
-            },
-            {
-                question : "Según Doctrina y Convenios 18, ¿qué se enseña sobre el arrepentimiento?",
-                options: [
-                    "(A) Es esencial para la salvación",
-                    "(B) Es opcional para los creyentes",
-                    "(C) Solo es necesario para los pecados graves",
-                    "(D) No es necesario si se tiene fe"
-                ],
-                "correct": 0
-            },
-            {
-                question : "En Doctrina y Convenios 27, ¿qué representa el pan y el vino en la Santa Cena?",
-                options: [
-                    "(A) La unidad de la Iglesia",
-                    "(B) El cuerpo y la sangre de Cristo ",
-                    "(C) La abundancia material",
-                    "(D) La resurrección futura"
-                ],
-                "correct": 1
-            },
-            {
-                question : "Según Doctrina y Convenios 1-30, ¿qué se enseña sobre la naturaleza de Dios?",
-                options: [
-                    "(A) Que Dios no tiene un cuerpo físico",
-                    "(B) Que Dios es un espíritu sin forma",
-                    "(C) Que Dios habita en un lugar desconocido",
-                    "(D) Que Dios tiene un cuerpo de carne y huesos "
-                ],
-                "correct": 3
-            },
-            {
-                question : "En Doctrina y Convenios 1-37, ¿qué se enseña sobre el bautismo por los muertos?",
-                options: [
-                    "(A) Es una práctica esencial para la salvación",
-                    "(B) Es opcional para los miembros de la Iglesia",
-                    "(C) Solo se realiza en templos dedicados",
-                    "(D) No es necesario en la actualidad"
-                ],
-                "correct": 0
-            }
-        
-
-
-
-    
+  {
+    question:
+      "En Doctrina y Convenios 28, ¿qué se enseña sobre la revelacion en la Iglesia?",
+    options: [
+      "(A) Solo el profeta puede recibir revelaciones para la Iglesia",
+      "(B) Todos los miembros pueden recibir revelaciones personales",
+      "(C) Los líderes locales tienen la misma autoridad que el profeta",
+      "(D) La autoridad se basa en la sabiduría humana",
+    ],
+    correct: 0,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 1-30 al 33, ¿qué caracteriza la palabra de Dios?",
+    options: [
+      "(A) Es un don temporal que cambia con el tiempo",
+      "(B) Es eterna y se cumple sin fallar",
+      "(C) Depende de la interpretación humana",
+      "(D) Es un concepto filosófico sin autoridad divina",
+    ],
+    correct: 1,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 4, ¿qué cualidades son necesarias para servir en la obra misional?",
+    options: [
+      "(A) Conocimiento científico y habilidades técnicas",
+      "(B) Riqueza, fama y poder",
+      "(C) Fe, esperanza, caridad y amor",
+      "(D) Experiencia en liderazgo empresarial",
+    ],
+    correct: 2,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 1:37, ¿qué se nos exhorta a hacer con los mandamientos del Señor?",
+    options: [
+      "(A) Escudriñarlos diligentemente porque son verdaderos y sus promesas se cumplirán todas",
+      "(B) Examinarlos cuidadosamente porque son fidedignos y sus profecías se podrian cumplir",
+      "(C) Analizarlos profundamente porque son inspirados y sus bendiciones son seguras",
+      "(D) Revisarlos constantemente porque son precisos y sus declaraciones son ciertas",
+    ],
+    correct: 0,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 6:36, ¿qué invitacion nos hace el Salvador?",
+    options: [
+      "(A) Obedecer los mandaminetos del Señor",
+      "(B) Hcaer lo corrceto en la Iglesia",
+      "(C) Mirar hacia Él en todo pensamiento",
+      "(D) Amarlo a Él  y al prójimo",
+    ],
+    correct: 2,
+  },
+  {
+    question: "Según Doctrina y Convenios 8 , ¿cómo se recibe la revelación?",
+    options: [
+      "(A) Mediante el estudio , fe y esperanza",
+      "(B) Mediante el poder del Espíritu Santo",
+      "(C) Mediante sueños y visiones como Josue",
+      "(D) Mediante la lógica y el razonamiento",
+    ],
+    correct: 1,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 10:20, ¿qué estrategia usa Satanás para destruir la obra de Dios?",
+    options: [
+      "(A) Incita a la iniquidad contra lo bueno",
+      "(B) Engañar a los líderes de la Iglesia",
+      "(C) Crear divisiones entre los miembros",
+      "(D)  Robar y alterar las escrituras",
+    ],
+    correct: 0,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 13, ¿qué autoridad fue restaurada por Juan el Bautista?",
+    options: [
+      "(A) El Sacerdocio Mosaico",
+      "(B) El Sacerdocio Aarónico ",
+      "(C) El Sacerdocio Abrahamico",
+      "(D) El Sacerdocio de Melquisedec",
+    ],
+    correct: 1,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 14, ¿qué promete el Señor a quienes trabajan en Su obra?",
+    options: [
+      "(A) Muchas bendiciones",
+      "(B) Salud en el ombligo",
+      "(C) Levantarce en la primera resurrección",
+      "(D) Salvacion sempiterna ",
+    ],
+    correct: 3,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 17, ¿qué se les prometió a los Tres Testigos del Libro de Mormón?",
+    options: [
+      "(A) Recibir revelacion para la Iglesia",
+      "(B) Ser escribientes del libro de Mormón",
+      "(C) Poder traducir el libro de Mormón",
+      "(D) Ver las planchas de oro ",
+    ],
+    correct: 3,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 18:9-16, ¿qué enseña el Señor sobre el valor de las almas?",
+    options: [
+      "(A) Que todas las almas son iguales",
+      "(B) Que son de gran valor para Dios ",
+      "(C) Que solo algunas almas se salvaran",
+      "(D) Que el valor de las almas depende de sus obras",
+    ],
+    correct: 1,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 19, ¿qué advertencia se da sobre el pecado?",
+    options: [
+      "(A) Que el pecado es inevitable desde siempre",
+      "(B) Que el pecado no trae consecuencias",
+      "(C) Que el pecado trae sufrimiento y dolor ",
+      "(D) Que el pecado solo afecta a los hijos de Dios",
+    ],
+    correct: 2,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 23, ¿qué consejo da el Señor a los líderes de la Iglesia?",
+    options: [
+      "(A) Buscar revelacion para la Iglesia",
+      "(B) Ayudar a los miembros necesitados",
+      "(C) Exortar y fortalecer a la Iglesia ",
+      "(D) Interpretar el libro de Mormón",
+    ],
+    correct: 2,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 25, ¿cuál no es el papel de Emma Smith en la Iglesia?",
+    options: [
+      "(A) Ser una líder y ejemplo para las mujeres",
+      "(B) Ayudar y consolar al profeta José Smith",
+      "(C) Ser escribiente del libro de Mormón",
+      "(D) Explicar las escrituras y seleccionar Himnos",
+    ],
+    correct: 0,
+  },
+  {
+    question: "En Doctrina y Convenios 27, ¿qué simboliza el pan y el agua?",
+    options: [
+      "(A) Que estamos renovando los Convenios",
+      "(B) Que tomamos sobre nosotros su nombre",
+      "(C) La Expiación de Jesucristo",
+      "(D) El cuerpo y la sangre de  Jesucristo ",
+    ],
+    correct: 3,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 33, ¿qué llama el Señor a hacer a Parley P. Pratt y a Ziva Peterson?",
+    options: [
+      "(A) Predicar el evangelio a los Lamanitas",
+      "(B) Bautizar a los Lamanitas",
+      "(C) Recibir revelacion paera predicar el evangelio",
+      "(D) Orar y Ayunar por los Lamanitas",
+    ],
+    correct: 0,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 27: 1-5,  ¿Qué emblemas no debemos usar en la Santa Cena?",
+    options: [
+      "(A) Pan y Agua",
+      "(B) Galleta y Agua ",
+      "(C) Pan y jugo de Uva",
+      "(D) Pan y Vino",
+    ],
+    correct: 3,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 1-38, ¿qué enseña el Señor sobre su palabra?",
+    options: [
+      "(A) Que van condicionadas a la fe",
+      "(B) Que puede cambiar con el tiempo",
+      "(C) Se cumplirán todas ",
+      "(D) Que se cumple segun nuestra obediencia",
+    ],
+    correct: 2,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 19:6-12, ¿qué significa la palabra 'Castigo Eterno' ?",
+    options: [
+      "(A) Un tormento  eterno",
+      "(B) Un castigo pasajero",
+      "(C) Un período de tiempo corto ",
+      "(D) Un estado de separación parcial",
+    ],
+    correct: 0,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 19, ¿qué se enseña sobre el arrepentimiento?",
+    options: [
+      "(A) No es necesario si pagamos los diezmos",
+      "(B) Es solo para los miembros de la Iglesia",
+      "(C) Solo es necesario para los pecados graves",
+      "(D)  Es esencial para la salvación",
+    ],
+    correct: 3,
+  },
+  {
+    question:
+      "En Doctrina y Convenios 24:1-9, ¿qué pide el señor a Jose Smith en las aflicciones?",
+    options: [
+      "(A) Orar",
+      "(B) Ayunar ",
+      "(C) Ser paciente y soportar",
+      "(D) Ayunar y Orar",
+    ],
+    correct: 2,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 13:1, ¿qué llaves no tiene el sacerdocio Aronico?",
+    options: [
+      "(A) Las llaves de ministerio de ángeles",
+      "(B) las llaves del evangelio de arrepentimiento",
+      "(C) Las llaves  del bautismo",
+      "(D) Las llaves para comfirmar",
+    ],
+    correct: 3,
+  },
+  {
+    question:
+      "Según Doctrina y Convenios 21:4–8, ¿qué promesa recibe la iglesia si sigue las palabras del profeta?",
+    options: [
+      "(A) Las puertas del infierno no prevalecerán contra ellos",
+      "(B) Recibirán la bendiciones prometidas",
+      "(C) Serán exaltados en el postrer día",
+      "(D) Serán llamados hijos del convenio",
+    ],
+    correct: 0,
+  },
 ];
 
 export default questions;
